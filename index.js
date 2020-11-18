3e383c1567d707dd3e3664a7ecf4145d383b70e5
joe <zdnf_lgq@163.com>
2020-11-18 23:56:02 +0800

    add port

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const express = require("express");
 const app = new express();
 
 app.get("/test", (req, res) => {
-  res.end('aaa')
+  res.send("aaa");
 });
+const port = process.env.PORT || 5001;
 
-app.listen("3000");
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
