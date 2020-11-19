f9a3997d057f0cb116568ec2798a5594f8f252e0
joe <zdnf_lgq@163.com>
2020-11-19 23:56:10 +0800

    ejs

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
-const express = require("express");
+const Koa = require("koa");
+const KoaRouter = require("koa-router");
+const render = require("koa-ejs");
+const path = require("path");
+const static = require("koa-static")
 
-const app = new express();
+const app = new Koa();
+const router = new KoaRouter();
 
-app.get("/test", (req, res) => {
-  res.send("aaa");
+// 配置路由模块
+app.use(router.routes()).use(router.allowedMethods());
+app.use(static(__dirname+'/public'))
+
+//配置模板文件夹
+render(app, {
+  root: path.resolve(__dirname, "./views"),
+  layout: "layout",
+  viewExt: "html",
+  debug: false,
+  cache: false,
 });
+
+router.get("/", index);
+
+// 函数声明
+async function index(ctx) {
+  await ctx.render("index", {
+    title: "Things i love...."
+  });
+}
+
 const port = process.env.PORT || 5001;
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+app.listen(port, () => console.log("Server Started...."));
